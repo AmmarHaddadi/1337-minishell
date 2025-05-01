26906ac9cda92a9fa617653620aa6e63745b60f3
Ammar Haddadi <75220043+AmmarHaddadi@users.noreply.github.com>
2025-05-01 17:47:35 +0100

    +libft (fixed)

---
+++ src/libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaddadi <ahaddadi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 18:28:32 by ahaddadi          #+#    #+#             */
+/*   Updated: 2024/11/09 13:26:44 by ahaddadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	last->next = new;
+}
