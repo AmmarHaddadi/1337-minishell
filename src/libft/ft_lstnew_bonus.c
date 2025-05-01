ef88321b2f094fb00b7e839ddfcaf153aaa91875
Ammar Haddadi <75220043+AmmarHaddadi@users.noreply.github.com>
2025-05-01 17:47:35 +0100

    +libft (fixed)

---
+++ src/libft/ft_lstnew_bonus.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaddadi <ahaddadi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 15:14:09 by ahaddadi          #+#    #+#             */
+/*   Updated: 2024/11/09 13:12:39 by ahaddadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
+
+// #include <stdio.h>
+// int main()
+// {
+// 	t_list *list = ft_lstnew(NULL);
+// 	printf("%s\n", list->content);
+// 	return 0;
+// }
