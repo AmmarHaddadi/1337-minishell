b9766fe197965ce3a453dfdf749c1926c6bce870
Ammar Haddadi <75220043+AmmarHaddadi@users.noreply.github.com>
2025-06-16 20:57:04 +0100

    norm fixs

--- src/main.h
+++ src/main.h
@@ -70,5 +70,5 @@ void					ft_lstadd_back_cmd(t_command **lst, t_command *new);
 t_command				*split_commands_tokens(char *input, t_shellvar *vars);
 bool					all_whitespace(char *input);
 t_token					*tokens(char *input, t_shellvar *vars);
-void						prepare_heredocs(t_command *cmds);
+void					prepare_heredocs(t_command *cmds);
 #endif
