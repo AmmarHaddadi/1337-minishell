75d922746a1b83aad3c6437f3fa4349b45dcf359
Salah <ssallami@student.1337.ma>
2025-06-17 20:25:10 +0100

    fixed single and dbl quaot

--- src/parsing/replace.c
+++ src/parsing/replace.c
@@ -6,7 +6,7 @@
 /*   By: ssallami <ssallami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 16:34:13 by ssallami          #+#    #+#             */
-/*   Updated: 2025/06/17 17:02:26 by ssallami         ###   ########.fr       */
+/*   Updated: 2025/06/17 20:23:15 by ssallami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static int	update_quote_state(char **input, int *p, char c, t_quote *quote)
 		else if (*quote == dbl)
 			*quote = none;
 		else
-			*input[(*p)++] = '"';
+			(*input)[(*p)++] = '"';
 		return (true);
 	}
 	else if (c == '\'')
@@ -31,7 +31,7 @@ static int	update_quote_state(char **input, int *p, char c, t_quote *quote)
 		else if (*quote == single)
 			*quote = none;
 		else
-			*input[(*p)++] = '\'';
+			(*input)[(*p)++] = '\'';
 		return (true);
 	}
 	return (false);
@@ -50,7 +50,7 @@ char	*replace(char *input, t_shellvar *vars)
 	buffer[1] = malloc(sizeof(char) * (len_allocation(buffer[0], vars) + 1));
 	while (buffer[0][tab[0]])
 	{
-		if (update_quote_state(&buffer[0], &tab[1], buffer[0][tab[0]], &quote))
+		if (update_quote_state(&buffer[1], &tab[1], buffer[0][tab[0]], &quote))
 			tab[1] += 0;
 		else if (expand_dollar(buffer, tab, &quote, vars))
 			tab[1] += 0;
