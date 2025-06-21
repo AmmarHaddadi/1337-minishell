fe1dc5adff6ab82b7e46b81a7ba8c38be8e4cb2d
Ammar Haddadi <75220043+AmmarHaddadi@users.noreply.github.com>
2025-06-21 15:11:11 +0100

    fix #62 #66

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -17,28 +17,28 @@
 	reimplementing a Bash bug disguised as a "feature" is dumb
  */
 
-int	cd(t_command *command, char *home)
+int	cd(t_command *command, t_shellvar *vars)
 {
+	char	*path;
+	char	*old;
+
 	if (matrixlen(command->args) > 2)
+		return (ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO), 1);
+	else if (matrixlen(command->args) == 1)
 	{
-		ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO);
-		return (1);
-	}
-	if (matrixlen(command->args) == 1)
-	{
-		if (!home)
+		path = getvar("HOME", vars);
+		if (!path)
 			return (ft_putstr_fd("cd: $HOME not set\n", STDERR_FILENO), 1);
-		if (chdir(home) != 0)
-		{
-			perror("cd");
-			return (1);
-		}
-		return (0);
 	}
-	if (chdir(command->args[1]) != 0)
-	{
-		perror("cd");
-		return (1);
-	}
+	else if (!my_strcmp(command->args[1], "-"))
+		path = getvar("OLDPWD", vars);
+	else
+		path = command->args[1];
+	old = getcwd(NULL, 0);
+	if (chdir(path) != 0)
+		return (perror("cd"), 1);
+	updatevar("PWD", path, vars, true);
+	updatevar("OLDPWD", old, vars, true);
+	free(old);
 	return (0);
 }
